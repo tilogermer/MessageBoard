e7ca7baf5f86e64ba32e41cd297e76571dbfb722
Tilo Germer <tilo.germer@t-online.de>
2017-06-27 13:14:55 +0200

    added association between Message and User

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,11 +9,11 @@ def show
 end
 
 def new
-	@message = Message.new	
+	@message = current_user.messages.build	
 end
 
 def create
-	@message = Message.new(message_params)
+	@message = current_user.messages.build(message_params)
 	if @message.save
 		redirect_to root_path
 	else
