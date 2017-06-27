de387014640422d59c4ab30618a30e7dbe875a83
Tilo Germer <tilo.germer@t-online.de>
2017-06-27 16:57:49 +0200

    added actions to the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
+	before_action :find_message
+	before_action :find_comment, only: [:edit, :update, :destroy]
+	
 	def create
-		@message = Message.find(params[:message_id])
 		@comment = @message.comments.create(comment_params)
 		@comment.user_id = current_user.id
 
@@ -11,8 +13,25 @@ class CommentsController < ApplicationController
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @comment.update(comment_params)
+			redirect_to message_path(@message)
+		else
+			render 'edit'
+		end
+	end
+
 	private
 	def comment_params
 		params.require(:comment).permit(:content)
 	end
+	def find_message
+		@message = Message.find(params[:message_id])
+	end
+	def find_comment
+		@comment = @message.comments.find(params[:id])
+	end	
 end
